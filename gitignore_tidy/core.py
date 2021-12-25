1b7f81412131aec407964065e908750044afdf1e
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-12-25 22:34:45 +0100

    initial

---
+++ gitignore_tidy/core.py
@@ -0,0 +1,83 @@
+import os 
+PATH_GITIGNORE = ".gitignore"
+import logging
+import sys
+import re 
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.INFO)
+
+def tidy():
+    if not os.path.exists(PATH_GITIGNORE):
+        raise FileNotFoundError("No .gitignore file present.")
+
+    with open(PATH_GITIGNORE, 'r') as f:
+        lines = _normalize(f.read().splitlines())
+    
+    if len(lines) < 1:
+        logger.info(f"{PATH_GITIGNORE} is empty.")
+        return 
+
+    sorted_lines = _sort_lines(lines)
+    if sorted_lines == lines:
+        logger.info(f"{PATH_GITIGNORE} already tidy.")
+        return
+
+    with open(PATH_GITIGNORE, 'w') as f:
+        f.writelines([line + "\n" for line in sorted_lines])
+    logger.info(f"Succesfully written f{PATH_GITIGNORE}.")
+
+def _normalize(lines):
+    lines = [re.sub("^(!)? *\t*", "\\1", line) for line in lines]
+    lines = [re.sub(" *\t*$", "", line) for line in lines]
+    unique = []
+    for line in lines:
+        if line not in unique:
+            unique.append(line)
+    return unique
+
+def _sort_lines(lines):
+    original = lines
+    non_negated = [re.sub('^!', '', line) for line in lines]
+    sorted_non_negated = sorted(non_negated)
+    sorted_negated = []
+    for line in sorted_non_negated:
+        negated_cand = ("!" + line)
+        if negated_cand in original:
+            sorted_negated.append(negated_cand)
+        else:
+            sorted_negated.append(line)
+    
+    return sorted_negated
+
+            
+
+def _sort_lines1(lines):
+    unordered = dict()
+    if lines[0].startswith("!"):
+        raise ValueError(f"{PATH_GITIGNORE} files can't start with a exclusion.")
+    for l in lines:
+        if l.startswith("!"):
+            unordered[previous_l].append(l)
+        else:
+            unordered[l] = [l]
+            previous_l = l
+
+    keys = sorted(unordered.keys())
+    out = []
+    for key in keys:
+        values_in_key = unordered[key]
+        for value in values_in_key:
+            out.append(value)
+    
+    return out
+
+
+def order(seq): 
+    return [x for x, y in sorted(enumerate(seq), key=lambda x: x[1])]
+
+if __name__ == "__main__":
+    tidy()
+
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+setup(name='gitignore-tidy',
+      version='0.1',
+      description='Tidy up your .gitignore file',
+      author='Lorenz Walthert',
+      author_email='lorenz.walthert@icloud.com',
+      license='MIT',
+      packages=find_packages(),
+      entry_points = {
+        'console_scripts': ['gitignore-tidy=gitignore_tidy.core:tidy'],
+    },
+      zip_safe=False)
