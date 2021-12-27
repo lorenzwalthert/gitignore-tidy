c4c17035a1da5d81532f0914e6b116860a047a57
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-12-27 14:24:09 +0100

    add poetry

--- setup.py
+++ setup.py
@@ -1,15 +1,2 @@
-from setuptools import find_packages
 from setuptools import setup
-setup(
-    name='gitignore-tidy',
-    version='0.1',
-    description='Tidy up your .gitignore file',
-    author='Lorenz Walthert',
-    author_email='lorenz.walthert@icloud.com',
-    license='MIT',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': ['gitignore-tidy=gitignore_tidy.cli:main'],
-    },
-    python_requires='>=3.9',
-)
+setup()
