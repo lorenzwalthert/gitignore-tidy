d289e4fe1d4f495da1c71408ff3428e9f82ef845
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-12-27 11:28:53 +0100

    refactor

--- gitignore_tidy/core.py
+++ gitignore_tidy/core.py
@@ -8,34 +8,6 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.setLevel(logging.INFO)
 
 
-def tidy(files, allow_trailing_whitespace=False):
-    [tidy_one(file, allow_trailing_whitespace) for file in files]
-
-
-def tidy_one(file, allow_trailing_whitespace=False):
-    if not os.path.exists(file):
-        raise FileNotFoundError(f'{file} not found.')
-
-    with open(file) as f:
-        lines = _normalize(
-            f.read().splitlines(),
-            allow_trailing_whitespace=allow_trailing_whitespace,
-        )
-
-    if len(lines) < 1:
-        logger.info(f'{file} is empty.')
-        return
-
-    sorted_lines = _sort_lines_with_comments(lines)
-    if sorted_lines == lines:
-        logger.info(f'{file} already tidy.')
-        return
-
-    with open(file, 'w') as f:
-        f.writelines([line + '\n' for line in sorted_lines])
-    logger.info(f'Succesfully written {file}.')
-
-
 def _normalize(lines, allow_trailing_whitespace=False):
     if not allow_trailing_whitespace:
         lines = [re.sub('^(!)? *\t*', '\\1', line) for line in lines]
@@ -62,9 +34,21 @@ def _sort_lines(lines):
     return sorted_negated
 
 
-def _sort_lines_with_comments(lines):
-    sections = _sections_split(lines)
-    lines_out = _sections_order(sections)
+def _sections_sort(sections):
+    lines_out = []
+
+    for section_title, section_content in sections.items():
+
+        if section_content['trailing_blanks'] > 0:
+            lines_out.append('')
+
+        if section_title.startswith('#'):
+            lines_out.append(section_title)
+
+        for line in _sort_lines(section_content['values']):
+            if line != '':
+                lines_out.append(line)
+
     return lines_out
 
 
@@ -89,19 +73,35 @@ def _sections_split(lines):
     return sections
 
 
-def _sections_order(sections):
-    lines_out = []
-
-    for section_title, section_content in sections.items():
-
-        if section_content['trailing_blanks'] > 0:
-            lines_out.append('')
-
-        if section_title.startswith('#'):
-            lines_out.append(section_title)
-
-        for line in _sort_lines(section_content['values']):
-            if line != '':
-                lines_out.append(line)
-
+def _sort_lines_with_comments(lines):
+    sections = _sections_split(lines)
+    lines_out = _sections_sort(sections)
     return lines_out
+
+
+def _tidy_one(file, allow_trailing_whitespace=False):
+    if not os.path.exists(file):
+        raise FileNotFoundError(f'{file} not found.')
+
+    with open(file) as f:
+        lines = _normalize(
+            f.read().splitlines(),
+            allow_trailing_whitespace=allow_trailing_whitespace,
+        )
+
+    if len(lines) < 1:
+        logger.info(f'{file} is empty.')
+        return
+
+    sorted_lines = _sort_lines_with_comments(lines)
+    if sorted_lines == lines:
+        logger.info(f'{file} already tidy.')
+        return
+
+    with open(file, 'w') as f:
+        f.writelines([line + '\n' for line in sorted_lines])
+    logger.info(f'Succesfully written {file}.')
+
+
+def tidy(files, allow_trailing_whitespace=False):
+    [_tidy_one(file, allow_trailing_whitespace) for file in files]
