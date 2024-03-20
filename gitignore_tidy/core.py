39d5c39bd11370c62874dbcb5c0efe910cf61d7b
Lorenz Walthert <lorenzwalthert@users.noreply.github.com>
2024-03-20 21:43:46 +0800

    refactor: add type hints to source code

--- gitignore_tidy/core.py
+++ gitignore_tidy/core.py
@@ -1,31 +1,30 @@
-import os
+import pathlib
 import re
-from pathlib import Path
-from typing import List
-from typing import Optional
+import typing
 
 import typer
+
 app = typer.Typer()
 
 
-def _normalize(lines, allow_leading_whitespace=False):
+def _normalize(lines: list[str], allow_leading_whitespace: bool = False) -> list[str]:
     if not allow_leading_whitespace:
-        lines = [re.sub('^(!)? *\t*', '\\1', line) for line in lines]
-    lines = [re.sub(' *\t*$', '', line) for line in lines]
+        lines = [re.sub("^(!)? *\t*", "\\1", line) for line in lines]
+    lines = [re.sub(" *\t*$", "", line) for line in lines]
     unique = []
     for line in lines:
-        if line not in unique or line == '':
+        if line not in unique or line == "":
             unique.append(line)
     return unique
 
 
-def _sort_lines(lines):
+def _sort_lines(lines: list[str]) -> list[str]:
     original = lines
-    non_negated = [re.sub('^!', '', line) for line in lines]
+    non_negated = [re.sub("^!", "", line) for line in lines]
     sorted_non_negated = sorted(non_negated)
     sorted_negated = []
     for line in sorted_non_negated:
-        negated_cand = ('!' + line)
+        negated_cand = "!" + line
         if negated_cand in original:
             sorted_negated.append(negated_cand)
         else:
@@ -34,60 +33,62 @@ def _sort_lines(lines):
     return sorted_negated
 
 
-def _sections_sort(sections):
+def _sections_sort(sections: dict[str, typing.Any]) -> list[str]:
     lines_out = []
 
     for section_title, section_content in sections.items():
 
-        if section_content['trailing_blanks'] > 0:
-            lines_out.append('')
+        if section_content["trailing_blanks"] > 0:
+            lines_out.append("")
 
-        if section_title.startswith('#'):
+        if section_title.startswith("#"):
             lines_out.append(section_title)
 
-        for line in _sort_lines(section_content['values']):
-            if line != '':
+        for line in _sort_lines(section_content["values"]):
+            if line != "":
                 lines_out.append(line)
 
     return lines_out
 
 
-def _sections_split(lines):
+def _sections_split(lines: list[str]) -> dict:
     sections = dict()
-    current_section = 'root'
+    current_section = "root"
     for idx, line in enumerate(lines):
-        if line.startswith('#'):
+        if line.startswith("#"):
             sections[line] = dict(values=[])
             current_section = line
-            if idx > 0 and lines[idx - 1] == '':
-                sections[current_section]['trailing_blanks'] = 1
+            if idx > 0 and lines[idx - 1] == "":
+                sections[current_section]["trailing_blanks"] = 1
             else:
-                sections[current_section]['trailing_blanks'] = 0
+                sections[current_section]["trailing_blanks"] = 0
         elif idx == 0:  # first line has no comment
             sections[current_section] = dict(
-                values=[line], trailing_blanks=0,
+                values=[line],
+                trailing_blanks=0,
             )
 
         else:
-            sections[current_section]['values'].append(line)
+            sections[current_section]["values"].append(line)
     return sections
 
 
-def _sort_lines_with_comments(lines):
+def _sort_lines_with_comments(lines: list[str]) -> list[str]:
     sections = _sections_split(lines)
     lines_out = _sections_sort(sections)
     return lines_out
 
 
-def _tidy_file(path, *, allow_leading_whitespace=False):
-    if not os.path.exists(path):
-        raise FileNotFoundError(f'{path} not found.')
+def _tidy_file(path: pathlib.Path, *, allow_leading_whitespace: bool = False):
 
-    with open(path) as f:
+    if not path.exists():
+        raise FileNotFoundError(f"{path} not found.")
+
+    with path.open("r") as f:
         lines = f.read().splitlines()
 
     if len(lines) < 1:
-        typer.echo(f'{path} is empty.')
+        typer.echo(f"{path} is empty.")
         return
 
     sorted_lines = _tidy_lines(
@@ -98,17 +99,21 @@ def _tidy_file(path, *, allow_leading_whitespace=False):
     _write_if_changed(sorted_lines, lines, path)
 
 
-def _write_if_changed(sorted_lines, lines, path):
+def _write_if_changed(
+    sorted_lines: list[str],
+    lines: list[str],
+    path: pathlib.Path,
+):
     if sorted_lines == lines:
-        typer.echo(f'{path} already tidy.')
+        typer.echo(f"{path} already tidy.")
         return
 
-    with open(path, 'w') as f:
-        f.writelines([line + '\n' for line in sorted_lines])
-        typer.echo(f'Successfully written {path}.')
+    with path.open("w") as f:
+        f.writelines([line + "\n" for line in sorted_lines])
+        typer.echo(f"Successfully written {path}.")
 
 
-def _tidy_lines(lines, *, allow_leading_whitespace):
+def _tidy_lines(lines: list[str], *, allow_leading_whitespace: bool):
     lines = _normalize(
         lines,
         allow_leading_whitespace=allow_leading_whitespace,
@@ -120,7 +125,7 @@ def _tidy_lines(lines, *, allow_leading_whitespace):
 
 @app.command()
 def tidy_files(
-    files: Optional[List[Path]] = typer.Argument(
+    files: typing.Optional[list[pathlib.Path]] = typer.Argument(
         None,
         help="""\
         Paths to one or more gitignore files.
@@ -130,16 +135,12 @@ def tidy_files(
     ),
     allow_leading_whitespace: bool = typer.Option(
         False,
-        help='Whether or not to allow trailing whitespaces in file names',
+        help="Whether or not to allow trailing whitespaces in file names",
     ),
-
 ):
     """
     Tidy .gitignore files
     """
     if files is None or len(files) < 1:
-        files = [Path('.gitignore')]
-    [
-        _tidy_file(file, allow_leading_whitespace=allow_leading_whitespace)
-        for file in files
-    ]
+        files = [pathlib.Path(".gitignore")]
+    [_tidy_file(file, allow_leading_whitespace=allow_leading_whitespace) for file in files]
